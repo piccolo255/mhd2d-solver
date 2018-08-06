1f4e29f2bbd911ec317c0e6f6b9a263fa8e57602
piccolo255 <piccolo255@gmail.com>
2018-08-06 20:53:17 +0900

    add: methods and member variables for ENO-Roe

--- spatialmethodenoroe.cpp
+++ spatialmethodenoroe.cpp
@@ -13,12 +13,21 @@ SpatialMethodEnoRoe::SpatialMethodEnoRoe
 )
    : SpatialMethodEno{ nx, ny, bufferWidth, dx, dy, boundary, gamma }
 {
+   VUF = createMatrices( PRB_DIM, nxTotal, nyTotal );
+   VUG = createMatrices( PRB_DIM, nxTotal, nyTotal );
+   F_  = createMatrices( PRB_DIM, nxTotal, nyTotal );
+   G_  = createMatrices( PRB_DIM, nxTotal, nyTotal );
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 SpatialMethodEnoRoe::~SpatialMethodEnoRoe
    (
 ){
+   freeMatrices( VUF );
+   freeMatrices( VUG );
+   freeMatrices( F_ );
+   freeMatrices( G_ );
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,6 +35,30 @@ t_status SpatialMethodEnoRoe::integrate
    ( t_matrices   U
    , t_matrices   UL
    , double      &dtIdeal
+){
+   return { true, ReturnStatus::ErrorNotImplemented, "ENO Roe method not yet implemented" };
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+t_status SpatialMethodEnoRoe::getNumericalFluxF
+   ( double    U1[8]
+   , double    U2[8]
+   , double    F[8][8]
+   , double    VUF[8][7]
+   , double    F_[8]
+   , double   &maxWaveSpeed
+){
+   return { true, ReturnStatus::ErrorNotImplemented, "ENO Roe method not yet implemented" };
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+t_status SpatialMethodEnoRoe::getNumericalFluxG
+   ( double    U1[8]
+   , double    U2[8]
+   , double    G[8][8]
+   , double    VUG[8][7]
+   , double    G_[8]
+   , double   &maxWaveSpeed
 ){
    return { true, ReturnStatus::ErrorNotImplemented, "ENO Roe method not yet implemented" };
 }
--- spatialmethodenoroe.hpp
+++ spatialmethodenoroe.hpp
@@ -24,6 +24,23 @@ class SpatialMethodEnoRoe : public SpatialMethodEno
    protected:
 
    private:
+      t_matrices VUF; // undivided differences, flux F
+      t_matrices VUG; // undivided differences, flux G
+      t_matrices F_;  // numerical flux F
+      t_matrices G_;  // numerical flux G
+
+      t_status getNumericalFluxF( double  U1[PRB_DIM]
+                                , double  U2[PRB_DIM]
+                                , double  F[PRB_DIM][8]
+                                , double  VUF[PRB_DIM][7]
+                                , double  F_[PRB_DIM]
+                                , double &maxWaveSpeed );
+      t_status getNumericalFluxG( double  U1[PRB_DIM]
+                                , double  U2[PRB_DIM]
+                                , double  G[PRB_DIM][8]
+                                , double  VUG[PRB_DIM][7]
+                                , double  G_[PRB_DIM]
+                                , double &maxWaveSpeed );
 };
 
 #endif // SPATIALMETHODENOROE_HPP
