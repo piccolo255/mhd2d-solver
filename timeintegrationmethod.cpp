2db88ae072e3a14c57ed4d8aed293a10d4287dcd
piccolo255 <piccolo255@gmail.com>
2018-08-06 16:02:47 +0900

    add: time integration - method to update dt fix: additional parameters to constructor of time integration class

--- timeintegrationmethod.cpp
+++ timeintegrationmethod.cpp
@@ -2,15 +2,23 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 TimeIntegrationMethod::TimeIntegrationMethod
-   ( size_t       nx
-   , size_t       ny
-   , size_t       bufferWidth
+   ( size_t nx
+   , size_t ny
+   , size_t bufferWidth
+   , double dtMin
+   , double dtMax
+   , double cflNumber
+   , std::unique_ptr<SpatialIntegrationMethod> method
 )
    : nxProblem{ nx } , nyProblem{ ny }
    , bufferWidth{ bufferWidth }
    , nxFirst{ 0 +bufferWidth   }, nyFirst{ 0 +bufferWidth   }
    , nxLast { nx+bufferWidth   }, nyLast { ny+bufferWidth   }
    , nxTotal{ nx+bufferWidth*2 }, nyTotal{ ny+bufferWidth*2 }
+   , dtMin{ dtMin }
+   , dtMax{ dtMax }
+   , cflNumber{ cflNumber }
+   , method{ std::move( method ) }
 {
    //ctor
 }
@@ -19,4 +27,54 @@ TimeIntegrationMethod::~TimeIntegrationMethod
    (
 ){
    //dtor
+}
+
+t_status TimeIntegrationMethod::updateDt
+   ( double   &dtCurrent
+   , double    dtIdeal
+){
+   auto dt = dtCurrent;
+
+   // resize dt to fit between 0.5*cfl and cfl
+   while( dt < 0.5*cflNumber*dtIdeal && dt <= dtMax ){
+      dt *= 2.0;
+
+      #ifdef DEBUG_DT_UPDATE
+         OUT << "*** DEBUG: raised dt to " << dt << LF;
+      #endif // DEBUG_DT_UPDATE
+   }
+
+   while( dt > cflNumber*dtIdeal && dt >= dtMin ){
+      dt /= 2.0;
+
+      #ifdef DEBUG_DT_UPDATE
+         OUT << "*** DEBUG: lowered dt to " << dt << LF;
+      #endif // DEBUG_DT_UPDATE
+   }
+
+   // clamp from above at dtMax
+   if( dt > dtMax ){
+      dt = dtMax;
+   }
+
+   // error if < dtMin
+   if( dt < dtMin ){
+      return { true, ReturnStatus::ErrorTimeUnderflow
+             , "smallest allowed dt does not satisfy CFL condition\n! updateDt" };
+   }
+
+   if( dt == dtCurrent ){
+      #ifdef DEBUG_DT_UPDATE
+         OUT << "*** DEBUG: dt unchanged, dt = " << dt << LF;
+      #endif // DEBUG_DT_UPDATE
+
+      return { false, ReturnStatus::NoChange, "" };
+   } else {
+      #ifdef DEBUG_DT_UPDATE
+         OUT << "*** DEBUG: dt updated, dt = " << dt << LF;
+      #endif // DEBUG_DT_UPDATE
+      dtCurrent = dt;
+
+      return { false, ReturnStatus::Updated, "" };
+   }
 }
--- timeintegrationmethod.hpp
+++ timeintegrationmethod.hpp
@@ -2,31 +2,46 @@
 #define TIMEINTEGRATIONMETHOD_HPP
 
 #include "mhd2d.hpp"
+#include "spatialintegrationmethod.hpp"
 
 class TimeIntegrationMethod
 {
    public:
-      explicit TimeIntegrationMethod( size_t       nx
-                                    , size_t       ny
-                                    , size_t       bufferWidth
+      explicit TimeIntegrationMethod( size_t nx
+                                    , size_t ny
+                                    , size_t bufferWidth
+                                    , double dtMin
+                                    , double dtMax
+                                    , double cflNumber
+                                    , std::unique_ptr<SpatialIntegrationMethod> method
                                     );
       virtual ~TimeIntegrationMethod();
 
-      virtual t_status step( t_matrices    U
-                           , double       &current_dt
+      virtual t_status step( t_matrices   U
+                           , double      &dtCurrent
                            ) = 0;
 
    protected:
-      const size_t      nxProblem;
-      const size_t      nyProblem;
-      const size_t      bufferWidth;
+      const size_t   nxProblem;
+      const size_t   nyProblem;
+      const size_t   bufferWidth;
 
-      const size_t nxFirst;
-      const size_t nyFirst;
-      const size_t nxLast;
-      const size_t nyLast;
-      const size_t nxTotal;
-      const size_t nyTotal;
+      const size_t   nxFirst;
+      const size_t   nyFirst;
+      const size_t   nxLast;
+      const size_t   nyLast;
+      const size_t   nxTotal;
+      const size_t   nyTotal;
+
+      double   dtMin;
+      double   dtMax;
+      double   cflNumber;
+
+      std::unique_ptr<SpatialIntegrationMethod> method;
+
+      virtual t_status updateDt( double  &dtCurrent
+                               , double   dtIdeal
+                               );
 
    private:
 };
